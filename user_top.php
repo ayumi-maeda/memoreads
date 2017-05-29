cd33835cd79283b03cbd0ad32a2f03e7bc55cbdb
ayumi-maeda <maeda_peace4096@yahoo.co.jp>
2017-05-29 21:39:15 +0800

    モックほぼ完成。初プッシュ

---
+++ user_top.php
@@ -0,0 +1,376 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Company-HTML Bootstrap theme</title>
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+	<link rel="stylesheet" href="css/font-awesome.min.css">
+	<link rel="stylesheet" href="css/animate.css">
+	<link href="css/prettyPhoto.css" rel="stylesheet">
+	<link href="css/style.css" rel="stylesheet" />	
+	<link href="css/ayumi_edit.css" rel="stylesheet" />	
+    <!-- =======================================================
+        Theme Name: Company
+        Theme URL: https://bootstrapmade.com/company-free-html-bootstrap-template/
+        Author: BootstrapMade
+        Author URL: https://bootstrapmade.com
+    ======================================================= -->
+  </head>
+  <body>
+	<header>		
+		<nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+			<div class="navigation">
+				<div class="container">					
+					<div class="navbar-header">
+						<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse.collapse">
+							<span class="sr-only">Toggle navigation</span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+						</button>
+						<div class="navbar-brand">
+							<a href="index.html"><h1><span>Com</span>pany</h1></a>
+						</div>
+					</div>
+					
+					<div class="navbar-collapse collapse">							
+						<div class="menu">
+							<ul class="nav nav-tabs" role="tablist">
+								<li role="presentation"><a href="index.html" class="active">Home</a></li>
+								<li role="presentation"><a href="about.html">About Us</a></li>
+								<li role="presentation"><a href="services.html">Services</a></li>								
+								<li role="presentation"><a href="portfolio.html">Portfolio</a></li>
+								<li role="presentation"><a href="blog.html">Blog</a></li>
+								<li role="presentation"><a href="contact.html">Contact</a></li>						
+							</ul>
+						</div>
+					</div>						
+				</div>
+			</div>	
+		</nav>		
+	</header>
+<div class="rgba"></div>
+	<div class="feature">
+		<div class="text-center" >
+				<img src="images/cooltext245006182452535.png" >
+			</div>
+
+			
+		<div class="container rankings">
+			<div class="text-center">
+				<div class="col-lg-6">
+						<!-- rankig -->
+					
+					<div class="hi-icon-wrap hi-icon-effect wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="300ms" >
+					  <div><img src="images/cooltext245006766393490.png"></div> 
+					  <div><img src="images/gold.JPG" style="margin-right:440px;"></div>
+						
+
+						<!-- book部門１位 -->
+							<div class="container">
+							    <div class="row">
+							        <div class="col-xs-12 col-sm-5 col-md-6">
+							            <div class="well well-sm" style="margin-top: -30px;">
+							                <div class="row">
+							                    <div class="col-sm-6 col-md-4">
+							                    	
+							                        <img src="images/ダウンロード.jpeg" alt="" class="img-rounded img-responsive" />
+							                    </div>
+							                    <div class="col-sm-6 col-md-8">
+							                        <h4>
+							                            BOOK TITLE</h4>
+							                        <p>著者
+							                        </p>
+							                        <P>TEXTTEXTTEXTTEXT.....</P>
+							                        
+							                    </div>
+							                </div>
+							            </div>
+							        </div>
+							    </div>
+							</div>
+					</div>
+				</div>
+
+
+
+				<!-- ユーザー部門１位 -->
+
+				<div class="col-lg-6">
+					
+					
+					<div class="hi-icon-wrap hi-icon-effect wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="600ms" >
+					 <div><img src="images/cooltext245006698732627.png"></div>
+						<div><img src="images/gold.JPG" style="margin-right: 440px;"></div>
+						<div class="container">
+							    <div class="row">
+							        <div class="col-xs-12 col-sm-5 col-md-6">
+							            <div class="well well-sm">
+							                <div class="row">
+							                    <div class="col-sm-6 col-md-4">
+							                        <img src="images/ダウンロード.jpeg" alt="" class="img-rounded img-responsive" />
+							                    </div>
+							                    <div class="col-sm-6 col-md-8">
+							                        <h4>
+							                            BOOK TITLE</h4>
+							                        <p>著者
+							                        </p>
+							                        <P>TEXTTEXTTEXTTEXT.....</P>
+							                        
+							                    </div>
+							                </div>
+							            </div>
+							        </div>
+							    </div>
+							</div>
+
+						
+					</div>
+				</div>
+
+				<!-- book部門２位 -->
+				<div class="col-lg-6">
+					
+					<div class="hi-icon-wrap hi-icon-effect wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="900ms" >
+					<div><img src="images/silver.JPG" style="margin-right:440px; "></div>
+						<div class="container">
+
+
+							    <div class="row">
+							        <div class="col-xs-12 col-sm-5 col-md-6">
+							            <div class="well well-sm">
+							                <div class="row">
+							                    <div class="col-sm-6 col-md-4">
+							                    
+							                        <img src="images/ダウンロード.jpeg" alt="" class="img-rounded img-responsive" />
+							                    </div>
+							                    <div class="col-sm-6 col-md-8">
+							                        <h4>
+							                            BOOK TITLE</h4>
+							                        <p>著者
+							                        </p>
+							                        <P>TEXTTEXTTEXTTEXT.....</P>
+							                        
+							                    </div>
+							                </div>
+							            </div>
+							        </div>
+							    </div>
+							</div>
+					</div>
+				</div>
+
+				<!-- ユーザー部門２位 -->
+				<div class="col-lg-6">
+					<div class="hi-icon-wrap hi-icon-effect wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="1200ms" >
+					<div><img src="images/silver.JPG" style="margin-right: 440px;"></div>
+						<div class="container">
+							    <div class="row">
+							        <div class="col-xs-12 col-sm-5 col-md-6">
+							            <div class="well well-sm">
+							                <div class="row">
+							                    <div class="col-sm-6 col-md-4">
+							                        <img src="images/ダウンロード.jpeg" alt="" class="img-rounded img-responsive" />
+							                    </div>
+							                    <div class="col-sm-6 col-md-8">
+							                        <h4>
+							                            BOOK TITLE</h4>
+							                        <p>著者
+							                        </p>
+							                        <P>TEXTTEXTTEXTTEXT.....</P>
+							                        
+							                    </div>
+							                </div>
+							            </div>
+							        </div>
+							    </div>
+							</div>
+					</div>
+				</div>
+
+				<!-- book部門３位 -->
+				<div class="col-lg-6">
+					<div class="hi-icon-wrap hi-icon-effect wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="1200ms" >
+					 	<div><img src="images/bronz.JPG" style="margin-right: 440px;"></div>
+						<div class="container">
+							    <div class="row">
+							        <div class="col-xs-12 col-sm-5 col-md-6">
+							            <div class="well well-sm">
+							                <div class="row">
+							                    <div class="col-sm-6 col-md-4">
+							                        <img src="images/ダウンロード.jpeg" alt="" class="img-rounded img-responsive" />
+							                    </div>
+							                    <div class="col-sm-6 col-md-8">
+							                        <h4>
+							                            BOOK TITLE</h4>
+							                        <p>著者
+							                        </p>
+							                        <P>TEXTTEXTTEXTTEXT.....</P>
+							                        
+							                    </div>
+							                </div>
+							            </div>
+							        </div>
+							    </div>
+							</div>
+						
+					</div>
+				</div>
+
+				<!-- ユーザー部門３位 -->
+				<div class="col-lg-6">
+					<div class="hi-icon-wrap hi-icon-effect wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="1200ms" >
+					  <div><img src="images/bronz.JPG" style="margin-right: 440px;"></div>
+						<div class="container">
+							    <div class="row">
+							        <div class="col-xs-12 col-sm-5 col-md-6">
+							            <div class="well well-sm">
+							                <div class="row">
+							                    <div class="col-sm-6 col-md-4">
+							                        <img src="images/ダウンロード.jpeg" alt="" class="img-rounded img-responsive" />
+							                    </div>
+							                    <div class="col-sm-6 col-md-8">
+							                        <h4>
+							                            BOOK TITLE</h4>
+							                        <p>
+							                        	著者
+							                        </p>
+							                        <P>TEXTTEXTTEXTTEXT.....</P>
+							                        
+							                    </div>
+							                </div>
+							            </div>
+							        </div>
+							    </div>
+							</div>
+					</div>
+				</div>
+
+					<!-- book部門４位 -->
+				<div class="col-lg-6">
+					<div class="hi-icon-wrap hi-icon-effect wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="1200ms" >
+					<div><img src="images/yellow-number-4-icon-24402.png" style="margin-right:440px;"></div>
+						<div class="container">
+							    <div class="row">
+							        <div class="col-xs-12 col-sm-5 col-md-6">
+							            <div class="well well-sm">
+							                <div class="row">
+							                    <div class="col-sm-6 col-md-4">
+							                        <img src="images/ダウンロード.jpeg" alt="" class="img-rounded img-responsive" />
+							                    </div>
+							                    <div class="col-sm-6 col-md-8">
+							                        <h4>
+							                            BOOK TITLE</h4>
+							                        <p>
+							                        	著者
+							                        </p>
+							                        <P>TEXTTEXTTEXTTEXT.....</P>
+							                        
+							                    </div>
+							                </div>
+							            </div>
+							        </div>
+							    </div>
+							</div>
+						
+					</div>
+				</div>
+
+					<!-- ユーザー部門４位 -->
+				<div class="col-lg-6">
+					<div class="hi-icon-wrap hi-icon-effect wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="1200ms" >
+					  <div><img src="images/yellow-number-4-icon-24402.png" style="margin-right: 440px;"></div>
+						<div class="container">
+							    <div class="row">
+							        <div class="col-xs-12 col-sm-5 col-md-6">
+							            <div class="well well-sm">
+							                <div class="row">
+							                    <div class="col-sm-6 col-md-4">
+							                        <img src="images/ダウンロード.jpeg" alt="" class="img-rounded img-responsive" />
+							                    </div>
+							                    <div class="col-sm-6 col-md-8">
+							                        <h4>
+							                            BOOK TITLE</h4>
+							                        <p>
+							                        	著者
+							                        </p>
+							                        <P>TEXTTEXTTEXTTEXT.....</P>
+							                        
+							                    </div>
+							                </div>
+							            </div>
+							        </div>
+							    </div>
+							</div>
+						
+					</div>
+				</div>
+
+				 <!-- book部門５位 -->
+				<div class="col-lg-6">
+					<div class="hi-icon-wrap hi-icon-effect wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="1200ms" >
+						<div><img src="images/yellow-number-5-icon-24416.png" style="margin-right: 440px; "></div>
+							<div class="container">
+							    <div class="row">
+							        <div class="col-xs-12 col-sm-5 col-md-6">
+							            <div class="well well-sm">
+							                <div class="row">
+							                    <div class="col-sm-6 col-md-4">
+							                        <img src="images/ダウンロード.jpeg" alt="" class="img-rounded img-responsive" />
+							                    </div>
+							                    <div class="col-sm-6 col-md-8">
+							                        <h4>
+							                            BOOK TITLE</h4>
+							                        <p>
+							                        	著者
+							                        </p>
+							                        <P>TEXTTEXTTEXTTEXT.....</P>
+							                        
+							                    </div>
+							                </div>
+							            </div>
+							        </div>
+							    </div>
+							</div>
+						
+					</div>
+				</div>
+
+				 <!-- ユーザー部門５位 -->
+				<div class="col-lg-6">
+					<div class="hi-icon-wrap hi-icon-effect wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="1200ms" >
+					 <div><img src="images/yellow-number-5-icon-24416.png" style="margin-right: 440px;"></div>
+						<div class="container">
+							    <div class="row">
+							        <div class="col-xs-12 col-sm-5 col-md-6">
+							            <div class="well well-sm">
+							                <div class="row">
+							                    <div class="col-sm-6 col-md-4">
+							                        <img src="images/ダウンロード.jpeg" alt="" class="img-rounded img-responsive" />
+							                    </div>
+							                    <div class="col-sm-6 col-md-8">
+							                        <h4>
+							                            BOOK TITLE</h4>
+							                        <p>
+							                        	著者
+							                        </p>
+							                        <P>TEXTTEXTTEXTTEXT.....</P>
+							                        
+							                    </div>
+							                </div>
+							            </div>
+							        </div>
+							    </div>
+							</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+ </body>
+</html>
+	
